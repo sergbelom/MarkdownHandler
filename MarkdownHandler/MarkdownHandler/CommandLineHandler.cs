fe8c76d71cc9c46022cf9a4a3d76c0a3cdf8b61e
SergBelom <sbelomestnykh@softdev.spb.ru>
2020-02-11 18:59:35 +0300

    Added CommandLineParser.

---
+++ MarkdownHandler/MarkdownHandler/CommandLineHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace MarkdownHandler
+{
+    public class Options
+    {
+        [Option('h', "help", Default = false, Required = false, HelpText = "Help about console app.")]
+        public bool Help { get; set; }
+
+        [Option('r', "read", Required = false, HelpText = "Input files to be processed.")]
+        public IEnumerable<string> InputFiles { get; set; }
+
+        [Option('e', "count-elements", Required = false, HelpText = "Get report about count tables and figures.")]
+        public bool Count { get; set; }
+
+        [Option('c', "check-captions", Required = false, HelpText = "Check correct captions for figure and tables.")]
+        public bool Check { get; set; }
+    }
+
+    //class CommandLineHandler
+    //{
+    //    //cmds:
+    //    // --help - help 
+    //    // --input "*.md" - start read .md and .markdown file from current dir
+    //    // --countelem - подсчет суммарного количества изображений и таблиц в файлах;
+    //    // --checkcaps - проверяет для каждого файла, есть ли у каждого изображения или таблицы подпись вида "Рисунок 1. ....", "Таблица 2. ....", выводит список неподписанных;
+    //    // --quit - quit from console app
+    //}
+}
--- MarkdownHandler/MarkdownHandler/Program.cs
+++ MarkdownHandler/MarkdownHandler/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
 
 namespace MarkdownHandler
 {
@@ -6,7 +10,44 @@ namespace MarkdownHandler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Help)
+                    {
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Help}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Help}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+        }
+
+        static void RunOptions(Options opts)
+        {
+            if (opts.Help)
+                PrintHelp();
+            //handle options
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+
+        private static void PrintHelp()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine("Hello, and welcome to the  console application.");
+            result.AppendLine("This application takes in a data file and attempts to import that data into our systems.");
+            result.AppendLine("Valid options are:");
+            //result.AppendLine(HelpText.AutoBuild(args));
+            result.AppendLine("Press any key to exit");
+
+            Console.Write(result);
         }
     }
 }
